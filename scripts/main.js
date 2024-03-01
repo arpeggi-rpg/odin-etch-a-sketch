6e5a542712a41339a782c2ac2fd3fe813acd44e5
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-03-01 18:22:37 +0000

    Reworked grid generation
    Grid generation now relies on flexbox for dynamic sizing and wrapping of grid squares

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,22 @@
 const gridContainer = document.querySelector('#gridContainer');
 
+const getContainerHeight = function (gridSize) {
+    return (gridContainer.offsetHeight / gridSize).toString();
+}
+
 const generateGrid = function (gridSize) {
     if (gridSize < 1 || gridSize >= 101) {
         alert('Invalid size! Grids must be between 1 and 100 in size');
         return;
     }
-    for (let i = 0; i <= gridSize; i++) {
-        let column = document.createElement('div');
-        column.class = 'column';
-        for (let j = 0; j <= gridSize; j++) {
-            let square = document.createElement('div');
-            square.class = 'square';
-            square.style.backgroundColor = 'red';
-            square.style.width = '16px';
-            square.style.height = '16px';
-            column.appendChild(square);
-        }
-        gridContainer.appendChild(column);
+    containerHeight = getContainerHeight(gridSize);
+    for (let i = 0; i < gridSize ** 2; i++) {
+        let square = document.createElement('div');
+        square.class = 'square';
+        square.style.backgroundColor = 'red';
+        square.style.width = containerHeight + 'px';
+        square.style.height = square.style.width;
+        gridContainer.appendChild(square);
     }
 }
 
